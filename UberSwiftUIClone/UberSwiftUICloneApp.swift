b87709c8fa6ab6e00a5a072da1c1939a59fe622b
Erikson Loures <erikson992@gmail.com>
2023-05-27 14:23:37 -0300

    Initial Commit

---
+++ UberSwiftUIClone/UberSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUICloneApp.swift
+//  UberSwiftUIClone
+//
+//  Created by Erik Loures on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
